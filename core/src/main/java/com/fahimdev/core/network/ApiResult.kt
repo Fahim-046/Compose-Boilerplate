fc5b88d2699cddd4417dfe0451191dc1e7a3fddd
Fahim-046 <fahimfardous8@gmail.com>
2025-08-03 01:26:48 +0600

    Feat: Set up api call.

--- core/src/main/java/com/fahimdev/core/network/ApiResult.kt
+++ core/src/main/java/com/fahimdev/core/network/ApiResult.kt
@@ -1,4 +1,4 @@
-package com.fahimdev.composeboilerplate.core.network
+package com.fahimdev.core.network
 
 sealed class ApiResult<out T> {
     data class Success<T>(val data: T) : ApiResult<T>()
--- core/src/main/java/com/fahimdev/core/network/SafeApiRequest.kt
+++ core/src/main/java/com/fahimdev/core/network/SafeApiRequest.kt
@@ -1,40 +1,35 @@
-package com.fahimdev.composeboilerplate.core.network
+package com.fahimdev.core.network
 
-import android.content.Context
 import android.util.Log
-import com.fahimdev.composeboilerplate.BuildConfig
-import okio.IOException
 import org.json.JSONException
 import org.json.JSONObject
 import retrofit2.Response
+import java.io.IOException
 import java.net.HttpURLConnection
 
 object SafeApiRequest {
-    suspend fun <T : Any?> apiRequest(
-        context: Context,
-        call: suspend () -> Response<T>
-    ): ApiResult<T?> {
+    suspend fun <T : Any?> apiRequest(call: suspend () -> Response<T>): ApiResult<T?> {
         return try {
             val response = call.invoke()
 
-            if (response.isSuccessful && response.code() >= HttpURLConnection.HTTP_OK && response.code() < HttpURLConnection.HTTP_MULT_CHOICE) {
+            if (response.isSuccessful &&
+                response.code() in HttpURLConnection.HTTP_OK until HttpURLConnection.HTTP_MULT_CHOICE
+            ) {
                 ApiResult.Success(response.body())
             } else {
                 val error = response.errorBody()?.string()
-
                 val message = StringBuilder()
                 error?.let {
                     try {
                         message.append(JSONObject(it).getString("message"))
-                    } catch (e: JSONException) {
-                    }
+                    } catch (e: JSONException) { /* Ignore */ }
                     message.append("\n")
                 }
                 message.append("Error Code: ${response.code()}")
 
                 ApiResult.Error(message.toString(), response.code())
-
             }
+
         } catch (e: IOException) {
             if (BuildConfig.DEBUG) {
                 Log.d("Network Error", "Network Error: ${e.localizedMessage}")
