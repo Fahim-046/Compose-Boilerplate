0885096880f35d1b086cd631905b6a75b4a6bcec
fahim-046 <fahimfardous8@gmail.com>
2025-09-22 14:24:56 +0600

    Refactor: Configure network security and update Ktor logging.

--- data/src/main/java/com/fahimdev/data/datasource/remote/apiClient/ApiClient.kt
+++ data/src/main/java/com/fahimdev/data/datasource/remote/apiClient/ApiClient.kt
@@ -29,7 +29,7 @@ class ApiClient(
 ) {
     val client = HttpClient(OkHttp) {
         install(Logging) {
-            level = LogLevel.ALL
+            level = LogLevel.BODY
         }
 
         install(ContentNegotiation) {
