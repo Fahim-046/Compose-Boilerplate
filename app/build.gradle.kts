740a84848b941b6ece075e8a57910ebe6ee28286
fahim-046 <fahimfardous8@gmail.com>
2025-10-15 23:26:18 +0600

    chore: Enable release signing in Gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
